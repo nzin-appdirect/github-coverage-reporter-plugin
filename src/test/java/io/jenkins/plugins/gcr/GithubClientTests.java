562865e15c584eb27f02d77b05e9779b2b2ea871
Nicolas Zin <nicolas.zin@appdirect.com>
2019-07-30 09:39:28 -0400

    DP-2125: a CHANGE_ID can have either a CHANGE_URL or a GIT_URL

--- src/test/java/io/jenkins/plugins/gcr/GithubClientTests.java
+++ src/test/java/io/jenkins/plugins/gcr/GithubClientTests.java
@@ -47,9 +47,45 @@ public class GithubClientTests {
     }
 
     @Test
-    public void testGithubClient() throws Exception {
+    public void testGithubClientHttp() throws Exception {
 
-        PluginEnvironment mockEnvironment = createMockEnvironment();
+        PluginEnvironment mockEnvironment = createHttpMockEnvironment();
+        HttpClientMock mockClient = new HttpClientMock();
+        GithubPayload payload = createMockPayload();
+
+        File mockFile = TestUtils.loadResource("github-pr.json");
+
+        System.out.println("file content: "+Files.toString(mockFile, Charset.forName("utf-8")));
+
+        String url = String.format("https://api.github.com/repos/%s/pulls/%s", REPO, PULL_ID);
+        mockClient.onGet(url)
+                .withParameter("access_token", ACCESS_TOKEN)
+                .doReturn(200,Files.toString(mockFile, Charset.forName("utf-8")));
+
+        url = String.format("https://api.github.com/repos/%s/statuses/%s", REPO, COMMIT_HASH);
+
+        mockFile = TestUtils.loadResource("github-status.json");
+
+        mockClient.onPost(url)
+                .withParameter("access_token", ACCESS_TOKEN)
+                .doReturn(201,Files.toString(mockFile, Charset.forName("utf-8")));
+
+        GithubClient client = new GithubClient(mockEnvironment, null, ACCESS_TOKEN, mockClient);
+
+        try {
+            client.sendCommitStatus(payload);
+        } catch (GithubClientException ex) {
+            StringWriter sw = new StringWriter();
+            ex.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Assert.fail(exceptionAsString);
+        }
+    }
+
+    @Test
+    public void testGithubClientGit() throws Exception {
+
+        PluginEnvironment mockEnvironment = createGitMockEnvironment();
         HttpClientMock mockClient = new HttpClientMock();
         GithubPayload payload = createMockPayload();
 
@@ -93,7 +129,20 @@ public class GithubClientTests {
         return payload;
     }
 
-    private PluginEnvironment createMockEnvironment() {
+    private PluginEnvironment createGitMockEnvironment() {
+        EnvVars envVars = new EnvVars(
+                "GIT_URL", "git@github.com:" + REPO + ".git",
+                "CHANGE_ID","130",
+                "GIT_COMMIT", COMMIT_HASH,
+                "BUILD_URL", "https://my.build/url"
+        );
+
+        PluginEnvironment environment = new PluginEnvironment(envVars);
+
+        return environment;
+    }
+
+    private PluginEnvironment createHttpMockEnvironment() {
         EnvVars envVars = new EnvVars(
                 "CHANGE_URL", "https://github.com/" + REPO + "/pull/130",
                 "GIT_COMMIT", COMMIT_HASH,
