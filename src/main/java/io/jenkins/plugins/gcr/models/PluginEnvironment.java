4e1c77afbe5f50a27180a9fb46daa7ec9ce165de
Nicolas Zin <nicolas.zin@appdirect.com>
2019-07-30 09:39:28 -0400

    DP-2125: a CHANGE_ID can have either a CHANGE_URL or a GIT_URL

--- src/main/java/io/jenkins/plugins/gcr/models/PluginEnvironment.java
+++ src/main/java/io/jenkins/plugins/gcr/models/PluginEnvironment.java
@@ -23,15 +23,31 @@ public class PluginEnvironment {
             pullRequestRepository = get("ghprbGhRepository", env);
             gitHash = get("ghprbActualCommit", env);
         } else{
-            String changeUrl = get("CHANGE_URL", env);
+            // either we receive a http PR (like CHANGE_URL=https://github.com/<org>/<repo>/pull/<id>)
+            // or git PR (like GIT_URL=git@github.com:<org>/<repo>.git
+            if (env.containsKey("CHANGE_URL")) {
 
-            Pattern pattern = Pattern.compile("https://[^/]*/(.*?)/pull/(.*)");
-            Matcher matcher = pattern.matcher(changeUrl);
-            if (matcher.find()) {
-                pullRequestRepository = matcher.group(1);
-                pullId = matcher.group(2);
+                String changeUrl = get("CHANGE_URL", env);
+
+                Pattern pattern = Pattern.compile("https://[^/]*/(.*?)/pull/(.*)");
+                Matcher matcher = pattern.matcher(changeUrl);
+                if (matcher.find()) {
+                    pullRequestRepository = matcher.group(1);
+                    pullId = matcher.group(2);
+                } else {
+                    throw new IllegalArgumentException(String.format("Can't find the owner/repo from CHANGE_URL environmental variable '%s'", changeUrl));
+                }
             } else {
-                throw new IllegalArgumentException(String.format("Can't find the owner/repo from CHANGE_URL environmental variable '%s'", changeUrl));
+                String gitUrl = get("GIT_URL", env);
+
+                Pattern pattern = Pattern.compile("[^/]*@[^/]*:(.*?)\\.git");
+                Matcher matcher = pattern.matcher(gitUrl);
+                if (matcher.find()) {
+                    pullRequestRepository = matcher.group(1);
+                } else {
+                    throw new IllegalArgumentException(String.format("Can't find the owner/repo from GIT_URL environmental variable '%s'", gitUrl));
+                }
+                pullId = get("CHANGE_ID", env);
             }
         }
         buildUrl = get("BUILD_URL", env);
